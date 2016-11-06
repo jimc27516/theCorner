b407d5703f614acfe4442c491e40a9ae10d6db20
Jim Campbell <jcampbell@jcampbell-pro.local>
2016-11-06 13:23:51 -0500

    updating repo

---
+++ src/test/groovy/thecorner/RegisterControllerSpec.groovy
@@ -0,0 +1,46 @@
+package thecorner
+
+import grails.test.mixin.Mock
+import grails.test.mixin.TestFor
+import spock.lang.Specification
+
+/**
+ * See the API for {@link grails.test.mixin.web.ControllerUnitTestMixin} for usage instructions
+ */
+@TestFor(RegisterController)
+@Mock(Person)
+class RegisterControllerSpec extends Specification {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+        expect:"fix me"
+            true == false
+    }
+
+    void "test registerPerson submit action" () {
+        when:
+        params.userName = "jimbo"
+        params.password = "1234"
+        params.firstName = "Jim"
+        params.lastName = "Campbell"
+        params.favoriteFruit = "apple"
+
+        controller.registerPerson()
+
+        then:
+        view == '/theCorner/landing/LandingView'
+        params.userName == "jimbo"
+        params.password == "1234"
+        params.firstName == "Jim"
+        params.lastName == "Campbell"
+        params.favoriteFruit == "apple"
+        Person.count() == 1
+
+    }
+
+}
